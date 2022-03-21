85be930bc55a1fa1bcb61f85371050d645efa029
Musy0ka <musyokaallan96@gmail.com>
2022-03-21 09:44:22 +0300

    First commit

---
+++ delani.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    $(".clickable").click(function() {
+      $("#walrus-showing").toggle();
+      $("#walrus-hidden").toggle();
+    });
+  });
+
+  $(document).ready(function() {
+    $(".clickable1").click(function() {
+      $("#walrus-showing1").toggle();
+      $("#walrus-hidden1").toggle();
+    });
+  });
+
+  $(document).ready(function() {
+    $(".clickable2").click(function() {
+      $("#walrus-showing2").toggle();
+      $("#walrus-hidden2").toggle();
+    });
+  });
+
+  var form = document.getElementById("my-form");
+    
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
